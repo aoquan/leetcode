2a72b08b6004f438f359bdc37bb3eed4354de60f
aoquan <qaoimu@163.com>
2017-03-16 17:51:37 +0800

    30

---
+++ 30/30.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<map>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        map<string,int> mp;
+
+        
+    }
+};
+
+int main(){
+    return 0;
+}
